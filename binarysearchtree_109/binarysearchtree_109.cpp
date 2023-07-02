aed366db3bcbf5cfaeb4c8cb4a6ab8f7abb9664a
PutriHabibahDevina <putrihdm13@gmail.com>
2023-07-02 09:46:27 +0700

    mencari node yg spesifik di currentNode/parent

--- binarysearchtree_109/binarysearchtree_109.cpp
+++ binarysearchtree_109/binarysearchtree_109.cpp
@@ -55,4 +55,21 @@ public:
 			parent->rightchild = newNode; //Make the right child of the parent point to the new node
 		}
 	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+		//This function searchs the currentNode of the specified node as well as the current node 
+		//of it's parent
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
 };
